0548ccd45c5623b8f59feb0a4652af940f5ac6a3
Daniel O Sousa <daniel-sousa.ds@academico.ifpb.edu.br>
2023-10-09 18:31:26 -0300

    fix: recolocando o state de tecnologias que havia sido removido por engano

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
+import { useState } from "react";
+
 import { Header, List } from "./components";
 
 import './App.css';
 
 function App() {
+  const [ techs, setTechs ] = useState([]);
+
   return (
     <main>
       <Header updateTechs={ setTechs } />
