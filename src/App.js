7890e18b11aec707035d4d03dba50906400d4616
Daniel O Sousa <daniel-sousa.ds@academico.ifpb.edu.br>
2023-10-09 18:29:34 -0300

    env: removendo a array de dados mockados usada para testes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,8 @@
-import { useState } from "react";
-
 import { Header, List } from "./components";
 
 import './App.css';
 
 function App() {
-  const [ techs, setTechs ] = useState([
-    {
-      id: 0,
-      name: "Mussum Ipsum, cacilds vidis litro abertis. Pra lá, depois divoltis porris, paradis. Nullam volutpat risus nec leo commodo, ut interdum diam laoreet.",
-      done: false
-    },
-    {
-      id: 1,
-      name: "Mussum Ipsum, cacilds vidis litro abertis. Pra lá, depois divoltis porris, paradis. Nullam volutpat risus nec leo commodo, ut interdum diam laoreet.",
-      done: true
-    }
-  ]);
-
   return (
     <main>
       <Header updateTechs={ setTechs } />
